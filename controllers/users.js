7a95cce7d15d843d7febfc83d90c03e7b8a6a64f
Benik Sargsyan <beniksargsyan349@gmail.com>
2026-05-17 00:33:02 +0400

    added controllers/users.js logic

---
+++ controllers/users.js
@@ -0,0 +1,110 @@
+import moment from 'moment';
+import HttpErrors from 'http-errors';
+
+import Users from '../models/users.js';
+
+export default {
+  async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+
+      const user = await Users.findByEmail(email);
+
+      if (!user || (user.password !== Users.hashPassword(password))) {
+        throw new HttpErrors(401, {
+          errors: {
+            email: 'Invalid email or password',
+          }
+        });
+      }
+
+      const token = Users.encrypt({
+        userId: user.id,
+        expiresIn: moment().add(2, 'hour').toISOString(),
+      });
+
+      delete user.password;
+
+      res.json({
+        token,
+        user,
+      });
+    } catch (e) {
+      next(e);
+    }
+  },
+
+  async register(req, res, next) {
+    try {
+      const {email, password, name, age} = req.body;
+
+      if (await Users.checkEmailUnique(email)) {
+        throw new HttpErrors(422, {
+          errors: {
+            email: 'EMail is already in use',
+          },
+        });
+      }
+
+      const user = await Users.create({
+        name,
+        email,
+        age,
+        password: Users.hashPassword(password),
+      });
+
+      delete user.password;
+
+      res.json({
+        message: 'User registered successfully',
+        user,
+      });
+    } catch (e) {
+      next(e);
+    }
+  },
+
+  async profile(req, res, next) {
+    try {
+      const user = await Users.findById(req.userId);
+
+      delete user.password;
+
+      res.json({
+        user,
+      });
+    } catch (e) {
+      next(e);
+    }
+  },
+
+  async update(req, res, next) {
+    try {
+      const {name, age} = req.body;
+
+      const user = await Users.update(
+        req.userId,
+        {name, age},
+      );
+
+      res.json({
+        user,
+      });
+    } catch (e) {
+      next(e);
+    }
+  },
+
+  async getUsersList(req, res, next) {
+    try {
+      const data = await Users.getUsersList(
+        req.query.page,
+        req.query.limit,
+      );
+
+      res.json(data);
+    } catch (e) {
+      next(e);
+    }
+  }
+}
